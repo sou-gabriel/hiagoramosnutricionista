c52a916a039c895ac798c63890674a3a48e2b89f
Gabriel Ramos <dev.gabrielramos@gmail.com>
2023-04-29 15:05:35 -0300

    feat: create mobile-friendly about me section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,6 +48,45 @@ export default function Home() {
             priority
           />
         </section>
+
+        <section className="px-4 py-20">
+          <div className="flex flex-col items-center text-center">
+            <h2 className="font-bold uppercase leading-[130%] tracking-widest mb-1">
+              Sobre mim
+            </h2>
+            <strong className="text-2xl leading-[130%] mb-6 block">Hiago Ramos</strong>
+
+            <Image
+              alt="Foto do Hiago Ramos"
+              src="hiagoramos-about.svg"
+              width={358}
+              height={358}
+              className="mb-8"
+            />
+
+            <p className="text-sm leading-[22.4px] opacity-80 mb-6">
+              <Balancer>
+                Hiago Ramos é um nutricionista altamente qualificado e
+                apaixonado por promover hábitos alimentares saudáveis e
+                equilibrados em seus pacientes. Com anos de experiência na área,
+                Hiago acredita que a nutrição é fundamental para uma vida
+                saudável e busca constantemente se manter atualizado sobre as
+                últimas tendências e pesquisas em nutrição e saúde.
+              </Balancer>
+            </p>
+
+            <p className="text-sm leading-[22.4px] opacity-80">
+              <Balancer>
+                Seu objetivo é ajudar seus pacientes a atingirem seus objetivos
+                de saúde e bem-estar, fornecendo um atendimento personalizado e
+                adaptando as recomendações nutricionais às necessidades
+                individuais de cada pessoa. Hiago é um defensor da alimentação
+                natural e acredita que a dieta deve ser baseada em alimentos
+                integrais e frescos.
+              </Balancer>
+            </p>
+          </div>
+        </section>
       </main>
     </>
   );
